150c0a0a9f8cf9b59c85187b20130f836ea6d68c
Pietro <pietrosette@gmail.com>
2020-10-19 16:15:12 +0200

    relazioni one to one + seeders done

---
+++ app/Avatar.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Avatar extends Model
+{
+    //bisogna inserirlo per avvisare
+    //che abbiamo rimosso il timestamps
+    //nel migration
+    public $timestamps = false;
+    protected $fillable = [
+        'telefono', 'avatar', 'user_id',
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+}
